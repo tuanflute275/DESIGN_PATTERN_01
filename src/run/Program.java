13bed10ea362a64f4df34f22ec19b6dec16fdf0a
Nguyễn Văn Tuấn <122516850+tuanflute275@users.noreply.github.com>
2024-01-22 09:39:34 +0700

    Update Program.java

--- src/run/Program.java
+++ src/run/Program.java
@@ -7,11 +7,15 @@ import dao.impl.CategoryDAOImpl;
 import dao.impl.ProductDAOImpl;
 import entities.Category;
 import entities.Product;
+import factory.DAOFactory;
 
 public class Program {
 	public static void category() {
 		Scanner sc = new Scanner(System.in);
-		Category category = new Category();
+		Category category = new Category();		
+		CategoryDAOImpl cateDAO = (CategoryDAOImpl) DAOFactory.getDao(CategoryDAOImpl.class);
+		
+		
 		int choice_category = 5;
 		do {
 			System.out.println("\n======Quản lý danh mục======");
@@ -24,7 +28,7 @@ public class Program {
 			sc.nextLine();
 			switch (choice_category) {
 			case 1:
-				List<Category> list = CategoryDAOImpl.getInstance().get();
+				List<Category> list = cateDAO.get();
 				if (list.size() == 0) {
 					System.out.println("WARNING  ===> KHÔNG CÓ DỮ LIỆU.");
 				} else {
@@ -36,7 +40,7 @@ public class Program {
 				break;
 			case 2:
 				category.input();
-				boolean checkInsert = CategoryDAOImpl.getInstance().add(category);
+				boolean checkInsert = cateDAO.add(category);
 				if (checkInsert) {
 					System.out.println("\nSUCCESS ===> Thêm mới danh mục thành công.");
 				} else {
@@ -46,13 +50,13 @@ public class Program {
 			case 3:
 				System.out.print("Nhập ID cần cập nhật: ");
 				Integer idUpdate = sc.nextInt();
-				Category dataByIdUpdate = CategoryDAOImpl.getInstance().findId(idUpdate);
+				Category dataByIdUpdate = cateDAO.findId(idUpdate);
 				if (dataByIdUpdate == null) {
 					System.out.println("Không có danh mục nào với Id là: " + idUpdate);
 				} else {
 					dataByIdUpdate.display();
 					category.input();
-					boolean checkUpdate = CategoryDAOImpl.getInstance().edit(category);
+					boolean checkUpdate = cateDAO.edit(category);
 					if (checkUpdate) {
 						System.out.println("\nSUCCESS ===>Cập nhật danh mục thành công.");
 					} else {
@@ -64,7 +68,7 @@ public class Program {
 			case 4:
 				System.out.print("Nhập thông tin cần xóa: \n");
 				category.input();
-				boolean checkDelete = CategoryDAOImpl.getInstance().remove(category);
+				boolean checkDelete = cateDAO.remove(category);
 				if (checkDelete) {
 					System.out.println("\nSUCCESS ===>Xóa danh mục thành công.");
 				} else {
@@ -86,6 +90,7 @@ public class Program {
 	public static void product() {
 		Scanner sc = new Scanner(System.in);
 		Product product = new Product();
+		ProductDAOImpl proDAO = (ProductDAOImpl) DAOFactory.getDao(ProductDAOImpl.class);
 		int choice_product = 5;
 		do {
 			System.out.println("\n======Quản lý sản phẩm======");
@@ -98,7 +103,7 @@ public class Program {
 			sc.nextLine();
 			switch (choice_product) {
 			case 1:
-				List<Product> list = ProductDAOImpl.getInstance().get();
+				List<Product> list = proDAO.get();
 				if (list.size() == 0) {
 					System.out.println("WARNING  ===> KHÔNG CÓ DỮ LIỆU.");
 				} else {
@@ -110,7 +115,7 @@ public class Program {
 				break;
 			case 2:
 				product.input();
-				boolean checkInsert = ProductDAOImpl.getInstance().add(product);
+				boolean checkInsert = proDAO.add(product);
 				if (checkInsert) {
 					System.out.println("\nSUCCESS ===> Thêm mới sản phẩm thành công.");
 				} else {
@@ -120,13 +125,13 @@ public class Program {
 			case 3:
 				System.out.print("Nhập ID cần cập nhật: ");
 				Integer idUpdate = sc.nextInt();
-				Product dataByIdUpdate = ProductDAOImpl.getInstance().findId(idUpdate);
+				Product dataByIdUpdate = proDAO.findId(idUpdate);
 				if (dataByIdUpdate == null) {
 					System.out.println("Không có sản phẩm nào với Id là: " + idUpdate);
 				} else {
 					dataByIdUpdate.display();
 					product.input();
-					boolean checkUpdate = ProductDAOImpl.getInstance().edit(product);
+					boolean checkUpdate = proDAO.edit(product);
 					if (checkUpdate) {
 						System.out.println("\nSUCCESS ===>Cập nhật sản phẩm thành công.");
 					} else {
@@ -138,7 +143,7 @@ public class Program {
 			case 4:
 				System.out.print("Nhập thông tin cần xóa: \n");
 				product.input();
-				boolean checkDelete = ProductDAOImpl.getInstance().remove(product);
+				boolean checkDelete = proDAO.remove(product);
 				if (checkDelete) {
 					System.out.println("\nSUCCESS ===>Xóa sản phẩm thành công.");
 				} else {
